9ccf64a359d7a3e1546477bb7ac560de2fb7840a
Aserbekov Kuanyshgali <k.aserbekov@profsoft.pro>
2023-03-19 18:19:05 +0400

    Add Prefect pipeline

--- Dagster.py
+++ Dagster.py
@@ -8,19 +8,13 @@ path_in, path_out = "original.csv", "result.csv"
 def load_data():
     df = pd.read_csv(path_in)
     return df
-
-
 @asset
 def get_domain(load_data):
     load_data['domain'] = [urlparse(i).netloc for i in load_data['url']]
     return load_data
-
-
 @asset
 def upload_data(get_domain):
     get_domain.to_csv(path_out)
-
-
 def run_assets():
     materialize(assets=[load_data, get_domain, upload_data])
 
