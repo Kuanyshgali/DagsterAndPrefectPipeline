017ae537f55365b742213c4aac5b1eebca70d9ac
Aserbekov Kuanyshgali <k.aserbekov@profsoft.pro>
2023-03-19 18:12:42 +0400

    Init commit and add Dagster pipeline

---
+++ Dagster.py
@@ -0,0 +1,28 @@
+from dagster import asset, materialize, op
+import pandas as pd
+from urllib.parse import urlparse
+
+path_in, path_out = "original.csv", "result.csv"
+
+@asset
+def load_data():
+    df = pd.read_csv(path_in)
+    return df
+
+
+@asset
+def get_domain(load_data):
+    load_data['domain'] = [urlparse(i).netloc for i in load_data['url']]
+    return load_data
+
+
+@asset
+def upload_data(get_domain):
+    get_domain.to_csv(path_out)
+
+
+def run_assets():
+    materialize(assets=[load_data, get_domain, upload_data])
+
+if __name__ == '__main__':
+    run_assets()
